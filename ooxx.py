5264d118b7ad4644ba157ff23974da724005e900
chenzhiqian <chenzhiqian@ledongli.cn>
2014-05-08 14:30:51 +0800

    first version

---
+++ ooxx.py
@@ -0,0 +1,14 @@
+__author__ = 'Danny'
+
+import urllib
+import urllib2
+import cookielib
+
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+opener.addheaders = [('User-agent', 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)')]
+urllib2.install_opener(opener)
+req = urllib2.Request("http://www.baidu.com", urllib.urlencode({"username":"root", "password":"ROOTXXOO"}))
+req.add_header("Referer", "http://google.com")
+resp = urllib2.urlopen(req)
+print resp.read()
