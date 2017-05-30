c3ff88ce35f442cc21307bf79a8ac02fb44cb880
aquastar <aquastar@qq.com>
2017-05-30 15:40:57 -0400

    update

--- gen_csv_4_amt.py
+++ gen_csv_4_amt.py
@@ -1,3 +1,4 @@
+import os
 import random
 from glob import glob
 
@@ -7,8 +8,12 @@ base_music = glob('2/*/*')
 base_link = 'https://github.com/aquastar/URL_Test/blob/master/'
 para = '?raw=true'
 
-for _ in xrange(100):
-    t = random.choice(test_music)
-    b = random.choice(base_music)
-    print '{}{}{}'.format(base_link, t, para)
-    print '{}{}{}'.format(base_link, b, para)
+with open('amt_input.csv', "wb") as csv_file:
+    # write the header
+    csv_file.write('baseline_name,jazz_link,baseline_link\n')
+
+    for _ in xrange(100):
+        t = random.choice(test_music)
+        b = random.choice(base_music)
+        line = '{},{}{}{}, {}{}{}\n'.format(b.split(os.sep)[1], base_link, t, para, base_link, b, para)
+        csv_file.write(line)
