e1f1810f0bdf8e51b287c045ae77dbb4153a6d01
aquastar <qqadd520@126.com>
2018-04-05 17:55:54 -0400

    update

--- gen_csv_4_amt.py
+++ gen_csv_4_amt.py
@@ -8,9 +8,9 @@ base_music = glob('2/*/*')
 base_link = 'https://github.com/aquastar/URL_Test/blob/master/'
 para = '?raw=true'
 
-with open('amt_input.csv', "wb") as csv_file:
+with open('amt_input.csv', "w") as csv_file:
     # write the header
-    csv_file.write("baseline_name,jazz_link,baseline_link\n")
+    csv_file.write('baseline_name,jazz_link,baseline_link\n')
 
     for _ in range(150):
         t = random.choice(test_music)
---
+++ rst_stat.py
@@ -0,0 +1,46 @@
+import csv
+from collections import Counter
+import os
+
+stat = {}
+
+dir = 'amt_rst'
+for filename in os.listdir(dir):
+    file_path = dir + os.sep+ filename
+    with open(file_path, 'rt') as f:
+        reader = csv.reader(f)
+        next(reader)  # skip the header
+        included_cols = [-6, -3, -2]
+        for row in reader:
+            if True:  # row[-1] == 'jazz':
+                content = list(row[i] for i in included_cols)
+                # print(content)
+                if row[-6] not in stat:
+                    stat[row[-6]] = []
+                stat[row[-6]].append(row[-3:-1])
+
+for k, v in stat.items():
+    an_fusion = []
+    an_novel = []
+    for ans in v:
+        an_fusion.append(ans[0])
+        an_novel.append(ans[1])
+
+    len_fusion = len(an_fusion)
+    len_novel = len(an_novel)
+
+    cnt_an_fusion = Counter(an_fusion)
+    cnt_an_novel = Counter(an_novel)
+
+    print(k, "{}".format(len_novel))
+
+    print("[2nd question]")
+    for ele in cnt_an_fusion:
+        print("{}:{:.2f}".format(ele, float(cnt_an_fusion[ele]) / len_fusion), end='\t')
+    fl = 1 - (abs(cnt_an_fusion['jazz'] - cnt_an_fusion['folk']) + cnt_an_fusion['none']) / len_fusion
+    print('FL:{:.2f}'.format(fl))
+
+    print("\n[3rd question]")
+    for ele in cnt_an_novel:
+        print("{}:{:.2f}".format(ele, float(cnt_an_novel[ele]) / len_novel), end='\t')
+    print('\n')
